a000dcf8cb615b8e61cb0a36a47303d620d46dba
Kwanwoo Lee <vfz5za@virginia.edu>
2025-02-05 03:32:04 -0500

    Apportionment

---
+++ src/main/java/edu/virginia/sde/hw1/Apportionment.java
@@ -0,0 +1,7 @@
+package edu.virginia.sde.hw1;
+
+public class Apportionment {
+    public static void main(String[] args) {
+        System.out.println("Apportionment Program is about to start");
+    }
+}
